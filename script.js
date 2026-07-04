b90e8c691c4d3d912161998f7954bb48ab5f37ef
Niharika Alam <70300853+nalam004@users.noreply.github.com>
2026-07-03 20:00:52 -0400

    update navbar transition

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Navbar scroll animation
+const navbar = document.querySelector('header nav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
 const menuToggle = document.getElementById('menu-toggle');
 const navMenu = document.getElementById('primary-nav-menu');
 const menuIcon = menuToggle.querySelector('i');
