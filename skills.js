818316d8623c102fb67a65779974b47fa7f92f80
Niharika Alam <70300853+nalam004@users.noreply.github.com>
2026-07-09 22:37:48 -0400

    change to blue color theme

--- skills.js
+++ skills.js
@@ -2,56 +2,56 @@ const skills = [
     {
         "name": "HTML/CSS/SCSS",
         "type": "languages",
-        "icon": "https://img.icons8.com/ios-filled/500/6b2d43/html-5--v1.png"
+        "icon": "https://img.icons8.com/ios-filled/500/1c2b48/html-5--v1.png"
     },
     {
         "name": "Javascript",
         "type": "languages",
-        "icon": "https://img.icons8.com/ios-filled/500/6b2d43/javascript-logo.png"
+        "icon": "https://img.icons8.com/ios-filled/500/1c2b48/javascript-logo.png"
     },
     {
         "name": "JQuery",
         "type": "languages",
-        "icon": "https://img.icons8.com/ios-filled/500/6b2d43/jquery.png"
+        "icon": "https://img.icons8.com/ios-filled/500/1c2b48/jquery.png"
     },
     {
         "name": "Python",
         "type": "languages",
-        "icon": "https://img.icons8.com/ios-filled/500/6b2d43/python.png"
+        "icon": "https://img.icons8.com/ios-filled/500/1c2b48/python.png"
     },
     {
         "name": "ReactJS",
         "type": "framework",
-        "icon": "https://img.icons8.com/ios-filled/500/6b2d43/react-native.png"
+        "icon": "https://img.icons8.com/ios-filled/500/1c2b48/react-native.png"
     },
     {
         "name": "NextJS",
         "type": "framework",
-        "icon": "https://img.icons8.com/ios-filled/500/6b2d43/react-native.png"
+        "icon": "https://img.icons8.com/ios-filled/500/1c2b48/react-native.png"
     },
     {
         "name": "Visual Studio Code",
         "type": "tools",
-        "icon": "https://img.icons8.com/ios-filled/500/6b2d43/visual-studio.png"
+        "icon": "https://img.icons8.com/ios-filled/500/1c2b48/visual-studio.png"
     },
     {
         "name": "Figma",
         "type": "tools",
-        "icon": "https://img.icons8.com/ios-filled/500/6b2d43/figma--v1.png"
+        "icon": "https://img.icons8.com/ios-filled/500/1c2b48/figma--v1.png"
     },
     {
         "name": "Version Control",
         "type": "tools",
-        "icon": "https://img.icons8.com/ios-filled/500/6b2d43/git.png"
+        "icon": "https://img.icons8.com/ios-filled/500/1c2b48/git.png"
     },
     {
         "name": "Drupal",
         "type": "CMS",
-        "icon": "https://img.icons8.com/ios-filled/500/6b2d43/drupal.png"
+        "icon": "https://img.icons8.com/ios-filled/500/1c2b48/drupal.png"
     },
     {
         "name": "Docker Docksal",
         "type": "tools",
-        "icon": "https://img.icons8.com/ios-filled/500/6b2d43/docker.png"
+        "icon": "https://img.icons8.com/ios-filled/500/1c2b48/docker.png"
     }
 ];
